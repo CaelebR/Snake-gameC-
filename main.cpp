9c86fdb428b226d4725d97568c37f4fc60ef4675
CaelebR <caelebrenner@gmail.com>
2025-11-10 16:04:08 -0800

    added Snake class and started movment logic

--- main.cpp
+++ main.cpp
@@ -1,15 +1,42 @@
 #include <iostream>
 #include <raylib.h>
+#include <deque>
+#include <raymath.h>
 
 using namespace std;
 
-Color green = {255, 57, 255, 255};
-Color red = {255, 0, 0, 255};
+Color green = {57, 255, 20, 255};
 
 int cellSize = 30;
 int cellCount = 25;
 
-class Food {
+
+class Snake 
+{
+    public :
+        deque<Vector2> body = {Vector2{6,9}, Vector2{5,9}, Vector2{4,9}};
+        Vector2 direction = {1,0};
+
+        void Draw() {
+            for (unsigned int i = 0; i < body.size(); i++)
+            {
+                float x = body[i].x;
+                float y = body[i].y;
+                Rectangle segment = {x * cellSize, y * cellSize, (float)cellSize, (float)cellSize};
+                DrawRectangleRounded(segment, 0.5, 6, green);
+            }
+                
+        }
+
+        void Update()
+        {
+            body.pop_back();
+            body.push_front(Vector2Add(body[0], direction));
+        }
+};
+
+class Food 
+{
 public:
     Vector2 position;
     Texture2D texture;
@@ -48,11 +75,13 @@ int main () {
     InitWindow(cellSize*cellCount, cellSize*cellCount, "Snake Game");
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
     Food food = Food();
+    Snake snake = Snake();
     while(WindowShouldClose() == false)
     {
         BeginDrawing();
 
         food.Draw();
+        snake.Draw();
 
 
 
